4f682570b7358a8583765a52c48211382866b6cf
unknown <eugene.vedensky@gmail.com>
2018-07-02 22:33:51 -0400

    added react router, started fleshing out state for register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,29 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          {/* <Landing /> */}
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/Auth/Register.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+    //some hacky stuff.
+    // Redefining the context of 'this' for onChange to be able to
+    // invoke the passed down function
+    // of setState from the reference to the react
+    // Component prototype
+
+    // this.onChange = this.onChange.bind(this);
+  }
+
+  //alt: onChange = e => this.setState({ [e.target.name]: e.target.value })
+  //this works because this in trad func scope points to containing object...since this is class
+  onSubmit = e => {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      password: this.state.password,
+      email: this.state.email,
+      password2: this.state.password2
+    };
+    console.log(newUser);
+  };
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+  //   onChange(e) {
+  //       this causes bugs because the context of this referring to the class is not perserved, therefore we need to bind this in the constructor
+  //     this.setState({ [e.target.name]: e.target.value });
+  //   }
+
+  render() {
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
